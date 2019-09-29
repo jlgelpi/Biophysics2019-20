04f0a28ad8f58bebda17d6c0d5f2fb47e488a2d4
Josep Ll. Gelpi <gelpi@ub.edu>
2019-09-29 19:48:55 +0200

    Clean style

--- forcefield.py
+++ forcefield.py
@@ -8,26 +8,24 @@ class VdwParamset():
     def __init__ (self, file_name):
         self.at_types = {}
         try:
-            fh = open(file_name,"r")
+            fh = open(file_name, "r")
         except OSError:
-            print ("#ERROR while loading parameter file (" ,file_name,")" )
+            print ("#ERROR while loading parameter file (", file_name, ")")
             sys.exit(2)
         for line in fh:
             if line[0] == '#':
                 continue
             data = line.split()
-            self.at_types[data[0]]=AtomType(data)
+            self.at_types[data[0]] = AtomType(data)
         self.ntypes = len(self.at_types)
         fh.close()
 
 class AtomType():
-    def __init__(self,data):
+    def __init__(self, data):
         self.id   = data[0]
         self.eps  = float(data[1])
         self.sig  = float(data[2])
         self.mass = float(data[3])
         self.fsrf = float(data[4])
         self.rvdw = self.sig * 0.5612
-
-
 
--- residue_library.py
+++ residue_library.py
@@ -7,31 +7,31 @@
 import sys
 
 class ResiduesDataLib():
-    def __init__(self,fname):
-        self.residue_data={}
+    def __init__(self, fname):
+        self.residue_data = {}
         try:
-            fh = open(fname,"r")
+            fh = open(fname, "r")
         except OSError:
-            print ("#ERROR while loading library file (" ,fname,")" )
+            print ("#ERROR while loading library file (", fname, ")")
             sys.exit(2)
         for line in fh:
             if line[0] == '#':
                 continue
             data = line.split()
             r = Residue(data)
-            self.residue_data[r.id]=r
+            self.residue_data[r.id] = r
         self.nres = len(self.residue_data)
 
-    def get_params (self,resid,atid):
-        if resid+':'+atid in self.residue_data:
-            return self.residue_data[resid+':'+atid]
+    def get_params (self, resid, atid):
+        atom_id = resid + ':' + atid
+        if atom_id in self.residue_data:
+            return self.residue_data[atom_id]
         else:
-            print ("WARNING: atom not found in library (",resid+':'+atid,')')
+            print ("WARNING: atom not found in library (", atom_id, ')')
             return None
 
 class Residue():
-    def __init__(self,data):
-        self.id     = data[0]+':'+data[1]
+    def __init__(self, data):
+        self .id = data[0] + ':' + data[1]
         self.at_type = data[2]
         self.charge  = float(data[3])
-
